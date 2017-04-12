04992363dabe0e6708958520c66558d39b164ac1
Runnan Zhong <r.tina.zhong@gmail.com>
2017-04-12 11:37:38 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,17 @@
+class PostValidator < ActiveModel::Validator
+  def validate(post)
+    unless post.title =~ /Won't Believe|Secret|Top \d|Guess/
+      post.errors[:title] << "Make it more clickbait-y!"
+    end
+  end
+end
+
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+
+  include ActiveModel::Validations
+  validates_with PostValidator
 end
